4da5f66e6c4c760544fcf268f9fbc31566872291
Hendra Dwi Saputra <hendra@usa.com>
2020-05-01 10:40:20 +0700

    Ubah nama header dan title di Django Admin

--- dasbor/urls.py
+++ dasbor/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = 'Ares Taliwang'
+admin.site.site_title = 'Ares Taliwang'
+admin.site.index_title = 'Administrasi Situs'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
