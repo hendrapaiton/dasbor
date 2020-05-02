57ef9a6d2b76555e76029797e798820021b5834a
Hendra Dwi Saputra <hendra@usa.com>
2020-05-02 10:35:07 +0700

    Collapse Options

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from blog.models import Blog, Comment
 
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+    fields = ('text', 'is_active',)
+    extra = 0
+    classes = ('collapse',)
+
+
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_created', 'last_modified', 'is_draft')
     list_filter = ('is_draft',)
@@ -11,7 +18,17 @@ class BlogAdmin(admin.ModelAdmin):
     list_per_page = 18
     actions = ('set_blogs_to_publish',)
     date_hierarchy = 'date_created'
-    fields = ('title', 'body', 'slug', 'is_draft')
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'slug'), 'body')
+        }),
+        ('Opsi lanjutan', {
+            'fields': ('is_draft',),
+            'description': 'Opsi untuk konfigurasi pembuatan slug',
+            'classes': ('collapse',)
+        }),
+    )
+    inlines = (CommentInline,)
 
     def get_ordering(self, request):
         if request.user.is_superuser:
@@ -25,5 +42,11 @@ class BlogAdmin(admin.ModelAdmin):
     set_blogs_to_publish.short_description = 'Tandai blog terpilih sebagai publik'
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['blog', 'text', 'date_created', 'is_active']
+    list_editable = ['is_active', ]
+    list_per_page = 18
+
+
 admin.site.register(Blog, BlogAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
